a70e9afa277a8b243c85c68295c1209fd0b96a88
ryuya <longyeguizhong@gmail.com>
2019-12-16 12:25:15 +0000

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-     before_action :set_task, only: [:show, :edit, :update, :destroy]
-    
+    before_action :correct_user, only: [:destroy]
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+
     def index
-         @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+      #@tasks = Task.order(id: :desc).page(params[:page]).per(3)
+        if logged_in?
+          @task = current_user.tasks.build  # form_with 用
+          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
 
     def show
@@ -15,15 +21,13 @@ class TasksController < ApplicationController
 
     def create
       @task = current_user.tasks.build(task_params)
-      @task = Task.new(task_params)
       if @task.save
         flash[:success] = 'Task が正常に登録されました'
-        redirect_to @task
-          
+        redirect_to root_url
       else
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         flash.now[:danger] = 'Task が登録されませんでした'
-        render 'tasks/index'
+        render :new
       end
     end
 
@@ -42,10 +46,8 @@ class TasksController < ApplicationController
 
     def destroy
         @task.destroy
-        
         flash[:success] = 'Task は正常に削除されました'
-        redirect_to messages_url
-        
+        redirect_to tasks_url
     end
     
     private
@@ -55,13 +57,11 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content, :status, :user)
+        params.require(:task).permit(:content, :status)
     end
     
-    
-    
     def correct_user
-      @task = currect_user.tasks.find_by(id: params[:id])
+      @task = current_user.tasks.find_by(id: params[:id])
       unless @task
         redirect_to root_url
       end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
   
@@ -11,7 +12,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  
   resources :tasks
   resources :tasks, only: [:create, :destroy]
   resources :users, only: [:index, :show, :new, :create]
